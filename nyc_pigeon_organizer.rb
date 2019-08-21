30ad3b9b97d6a1e16a66cbba9675471837fde97b
Arlene George <aemg@live.com>
2019-08-21 02:36:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,18 +1,18 @@
 def nyc_pigeon_organizer(data)
   pigeon_hash = {}
-  data.each do |discriptor, hash|
+  data.each do |descriptor, hash|
     hash.each do |quality, array|
       array.each do |name|
         if !pigeon_hash.has_key?(name)
           new_hash[name] = {}
         end
 
-        if !pigeon_hash[name].has_key?(property)
-          pigeon_hash[name][discriptor] = []
+        if !pigeon_hash[name].has_key?(descriptor)
+          pigeon_hash[name][descriptor] = []
         end
 
-        if !pigeon_hash[name][property].include?(attribute)
-          pi[name][discriptor] << quality.to_s
+        if !pigeon_hash[name][descriptor].include?(attribute)
+          pi[name][descriptor] << quality.to_s
         end
       end
     end
