58355a71c9e9b4f7a2a2366b466c73e5ae3c9fa9
Arlene George <aemg@live.com>
2019-08-21 02:47:58 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_hash = {}
+  data.each do |q, hash|
+    hash.each do |c, array|
+      array.each do |name|
+        if !pigeon_hash.has_key?(name)
+          pigeon_hash[name] = {}
+        end
+
+        if !pigeon_hash[name].has_key?(q)
+          pigeon_hash[name][q] = []
+        end
+
+        if !pigeon_hash[name][q].include?(c)
+          pigeon_hash[name][q] << c.to_s
+        end
+      end
+    end
+  end
+  pigeon_hash
 end
