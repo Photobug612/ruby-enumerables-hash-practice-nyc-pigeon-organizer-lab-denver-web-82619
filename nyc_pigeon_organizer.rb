4460b45e7002d91018cfb6378d3a774ca9f7860d
Arlene George <aemg@live.com>
2019-08-21 01:37:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,6 +6,6 @@ def nyc_pigeon_organizer(data)
         if sorted_pigeon_hash.include?(name)
           sorted_pigeon_hash[name] = {}
         end
-        if !sorted_pigeon_hash[name]include?
+        if !sorted_pigeon_hash[name]include?()
 
 end
